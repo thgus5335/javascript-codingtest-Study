1200029b064a34ef1e7490177c61c734c61f088a
JeongSabibi <jsbb528@naver.com>
2024-02-05 13:30:08 +0900

    Upload week2

---
+++ Level1/86051.js
@@ -0,0 +1,14 @@
+/**
+ * 없는 숫자 더하기
+ */
+function solution(numbers) {
+  let sum = 0;
+
+  for (let i = 0; i < 10; i++) {
+    if (!numbers.includes(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
