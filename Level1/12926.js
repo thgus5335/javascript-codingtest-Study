fc7ee872454692ab097bdaaf1dcd797423ce4837
JeongSabibi <jsbb528@naver.com>
2024-03-01 21:03:55 +0900

    💚 Upload week4
    Upload week4

--- Level1/12926.js
+++ Level1/12926.js
@@ -1,3 +1,23 @@
 /**
  * 시저 암호
  */
+function solution(s, n) {
+  let answer = s.split('');
+
+  answer = answer
+    .map((x) => {
+      let codeNum = x.charCodeAt();
+      if (x === ' ') return ' ';
+      if (codeNum >= 65 && codeNum <= 90)
+        return codeNum + n > 90
+          ? String.fromCharCode(codeNum + n - 26)
+          : String.fromCharCode(codeNum + n);
+      if (codeNum >= 97 && codeNum <= 122)
+        return codeNum + n > 122
+          ? String.fromCharCode(codeNum + n - 26)
+          : String.fromCharCode(codeNum + n);
+    })
+    .join('');
+
+  return answer;
+}
--- Level1/147355.js
+++ Level1/147355.js
@@ -1,3 +1,12 @@
 /**
  * 크기가 작은 부분문자열
  */
+function solution(t, p) {
+  let answer = 0;
+
+  for (let i = 0; i <= t.length - p.length; i++) {
+    if (+t.substr(i, p.length) <= +p) answer++;
+  }
+
+  return answer;
+}
